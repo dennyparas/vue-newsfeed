ac018992e11937197510ae26ce56654d025cbe69
daniel <dennyparas28@gmail.com>
2018-10-11 13:50:22 +0800

    initial template

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,25 @@
+<template>
+<footer class="footer">
+  <b-container class="text-center">
+    <span class="text-muted ">Vue Newsfeed</span>
+  </b-container>
+</footer>
+</template>
+<script>
+export default {
+  name: 'AppFooter',
+};
+</script>
+
+<style>
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  /* Set the fixed height of the footer here */
+  height: 50px;
+  line-height: 50px; /* Vertically center the text there */
+  background-color: #f5f5f5;
+}
+
+</style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,23 @@
+<template>
+<b-navbar toggleable="md" type="dark" variant="info">
+  <b-container>
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">VueFeed</b-navbar-brand>
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Link</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-container>
+</b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'AppNavbar',
+};
+</script>
+
+<style>
+
+</style>
